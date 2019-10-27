5e9d2ae9b0e39678e6a349244419a6b8ff0ff455
Rahul Sheth <56454700+rahulssheth@users.noreply.github.com>
2019-10-27 16:36:12 -0700

    User Login with Devise (#2)
    * Added in config, routes, and basic UI for login, creation, logout, deletion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_user!
+
+	def root
+		render file: 'layouts/application.html.erb'
+	end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+ config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  # Default URL Options for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }  
 
   config.action_mailer.perform_caching = false
 
