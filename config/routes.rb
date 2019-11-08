b3d80dea06b53452568ade8c3535d4d206c51d22
Rahul Sheth <56454700+rahulssheth@users.noreply.github.com>
2019-11-08 11:04:16 -0800

    Rahul/feed and profile (#7)
    Added seeds file and nil checks to feed
    
    Removed extraneous from landing page
    
    Fixed merge conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#root'
+  get '/', to: 'application#feed'
+  get '/user/:id', to: 'application#user'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+f_user = User.create(
+	name: 'User Feed Test 1',
+	email: 'test4@gmail.com',
+	profile_img_url: 'Headshot',
+	password: 'rand_string',
+)
+
+s_user = User.create(
+	name: 'User Feed Test 2',
+	email: 'test1@gmail.com',
+	profile_img_url: 'nosedive',
+	password: 'rand_string_2'
+)
