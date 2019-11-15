b431f0437423afd0b5d2b9c57687746b263a44b1
Rahul Sheth <Rahul@rahuls-mbp-3.lan>
2019-11-14 21:10:16 -0800

    Added in search and functionality to create user with name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def root
 		render file: 'layouts/application.html.erb'
 	end
 	def feed
 		@users = User.all
+		if params.has_key?(:filter)
+			filter_str = "%"
+			filter_str += params[:filter]
+			filter_str += '%'
+			@users = User.where("name like ?", filter_str)
+		end
 		render file: 'layouts/feed.html.erb'
 	end
 	def user
@@ -15,5 +22,17 @@ class ApplicationController < ActionController::Base
 		@comments.each { |a| sum += a.rating }
 		@aggregate_rating = (sum / @comments.size).round(1)
 		render file: 'layouts/user.html.erb'
+	end
+	def sign_up
+		console.log("here")
+		console.log(params)
+		User.create(name: params[:name], email: params[:email], password: params[:password])
+	end
+
+	protected
+	def configure_permitted_parameters
+	   attributes = [:name, :surname,:username, :email, :avatar]
+	   devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+	   
 	end
 end
